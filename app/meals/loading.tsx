43f50b1b2a48644fd531106c44cf47e8b600e608
Irakli Sarkisovi <irakli.sarkisovi@leverx.com>
2026-02-23 17:15:51 +0400

    feat: Loading implemented

---
+++ app/meals/loading.tsx
@@ -0,0 +1,9 @@
+import classes from './Loading.module.css'
+
+export default function Loading(){ 
+    return(
+        <>
+            <p className={classes.loading}>Loading Data...</p>
+        </>
+    )
+}
